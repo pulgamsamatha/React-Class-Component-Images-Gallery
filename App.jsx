d86fc97ed99607fd696e7d4e8ed89cc400565e2b
Samatha Pulgam <pulgamsamatha@gmail.com>
2025-10-23 21:09:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Gallery from './Components/Gallery'
+
+const App = () => {
+  return (
+    <div>
+      <Gallery/>
+    </div>
+  )
+}
+
+export default App
